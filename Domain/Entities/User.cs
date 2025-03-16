3aa1eab428767a2b57d17a697e0decc76dfdb560
kanto065 <kanto.uv@outlook.com>
2025-03-16 14:54:58 +0600

    fkey

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,15 @@ namespace Domain.Entities
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public string AddressLine { get; set; }
+        public virtual User User { get; set; }
     }
 }
